dc124f531ee3416e59932abf2d18dfe26c0fba04
Victor_Ote <victorotelakoski@gmail.com>
2026-05-28 06:32:05 -0300

    V.01

---
+++ core/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def contact(request):
+    return render(request, 'index.html')
+
+def product(request):
+    return render(request, 'index.html')
