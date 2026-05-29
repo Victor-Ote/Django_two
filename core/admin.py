f9156aeef510064d883fa1ce048efa85f4471f5a
Dental Market <victorotelakoski@gmail.com>
2026-05-29 10:49:40 -0300

    V 0.2

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product
+
+class AdminPrduct(admin.ModelAdmin):
+    list_display = ('title', 'stock')
+
+admin.site.register(Product)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField('title', max_length=100)
+    stock = models.IntegerField('stock', default=0)
+    
+    def __str__(self):
+        return self.title
+   
