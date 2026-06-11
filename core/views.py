8093e492647f69728138b1cecfd82c5fd89b1082
Dental Market <victorotelakoski@gmail.com>
2026-06-11 15:10:59 -0300

    Forms

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from .models import Product
+from .forms import ProductModelForm
 
 def index(request):
     return render(request, 'index.html')
@@ -10,9 +11,21 @@ def contact(request):
 
 def product(request):
     if request.method == 'POST':
-        print(request.POST)
-    
+        form = ProductModelForm(request.POST, request.FILES)
+        if form.is_valid():
+            print('Form is valid')
+            form.save()
+            msg = 'Product created successfully'
+        else:
+            msg = 'Form is not valid'
+            print('Form is not valid')
+            
+        print(f'Está sendo POST: {request.POST}')
+    else:
+        form = ProductModelForm()
+
     context = {
-        'products': Product.objects.all()
+        'form': form,
+        'msg': msg
     }
     return render(request, 'product.html', context)
