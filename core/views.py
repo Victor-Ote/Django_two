f62c1d07f575d021b233bf54291d181be36e2a1c
Dental Market <victorotelakoski@gmail.com>
2026-06-18 16:09:05 -0300

    Start DELETE

--- core/views.py
+++ core/views.py
@@ -10,22 +10,25 @@ def contact(request):
     return render(request, 'index.html')
 
 def product(request):
+    msg = ''
     if request.method == 'POST':
+        
         form = ProductModelForm(request.POST, request.FILES)
         if form.is_valid():
-            print('Form is valid')
             form.save()
             msg = 'Product created successfully'
         else:
             msg = 'Form is not valid'
             print('Form is not valid')
-            
-        print(f'Está sendo POST: {request.POST}')
+        form.clean()
+        print(f"Requisição:{request.POST.get('action')}")
+
     else:
         form = ProductModelForm()
 
     context = {
         'form': form,
-        'msg': msg
+        'msg': msg,
+        'products': Product.objects.all()
     }
     return render(request, 'product.html', context)
