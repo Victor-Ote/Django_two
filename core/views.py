dc0cd919ec564f7522be86de80b95931c56b0acf
Victor_Ote <victorotelakoski@gmail.com>
2026-06-23 07:06:28 -0300

    Start Auth

--- core/views.py
+++ core/views.py
@@ -47,12 +47,27 @@ class ProductListView(ListView):
     queryset = Product.objects.all()
     template_name = 'listProd.html'
     
+    def dispatch(self, request, *args, **kwargs):
+
+        if request.user.is_authenticated:
+            print(f"User logado: {request.user}")
+        else:
+            print("User não logado")
+        print("Entrou na view")
+        return super().dispatch(request, *args, **kwargs)
+
 #CREATE
 class ProductCreateView(CreateView):
     model = Product
     fields = '__all__'
     success_url = reverse_lazy('core:listProd')
     template_name = 'createProd.html'
+    
+    def post(self, request, *args, **kwargs):
+
+        print(f"User logado: {request.user}")
+
+        return super().post(request, *args, **kwargs)
 
 #UPDATE
 class ProductUpdateView(UpdateView):
